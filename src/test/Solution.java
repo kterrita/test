0301770023f3b47dc1a1e2991b87a64600c8b760
beleychev.ilya <beleychev.ilya@otr.ru>
2017-02-02 14:44:28 +0300

    commit

--- src/test/Solution.java
+++ src/test/Solution.java
@@ -5,10 +5,10 @@ package test;
  * @author beleychev.ilya 12.01.2017   17:23
  */
 public class Solution {
+
 	public static void main(String[] args) {
 		String s1 = "str";
-		String s2 = "str";
-		s2.intern();
-		System.out.println(s1 == s2);
+		String s2 = new String ("str");
+		System.out.println(s1 == s2.intern());
 	}
 }
