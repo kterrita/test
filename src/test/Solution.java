9999a0abbf42beb10e75886e4eeace6a69b225ac
beleychev.ilya <beleychev.ilya@otr.ru>
2017-02-02 15:00:52 +0300

    3rd commit

--- src/test/Solution.java
+++ src/test/Solution.java
@@ -5,10 +5,9 @@ package test;
  * @author beleychev.ilya 12.01.2017   17:23
  */
 public class Solution {
-
 	public static void main(String[] args) {
-		String s1 = "str";
-		String s2 = new String ("str");
+		String s1 = "qwe";
+		String s2 = new String ("qwe");
 		System.out.println(s1 == s2.intern());
 	}
 }
